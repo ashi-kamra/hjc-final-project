68c82278ec238fa39926c158307e39266f53e9cc
Ashi Kamra <ak118@wellesley.edu>
2025-03-28 20:03:55 -0400

    still getting same warning

---
+++ src/lib/Poverty.svelte
@@ -0,0 +1,20 @@
+<script>
+    import * as d3 from 'd3';
+    import {onMount} from "svelte";
+
+    let data = [];
+
+    // loading data
+    onMount(async () => {
+        data = await d3.csv("/2015-2019_ACS_Household_Income2.csv", row => ({
+            ...row,
+            location: row["Location"],
+            income: +row["Median Income"].replace("$", "").replace(',', '') // fixing formatting to remove $ or ,
+        }));
+        console.log(data);
+    });
+</script>
+
+<h1>
+    Testing 1,2,3...
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to HJC Final Project</h1>
+<a href="/src/lib/Poverty.svelte">Poverty Viz</a>
